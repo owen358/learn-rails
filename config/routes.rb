dfebedd977ff131a295a45231a9a86fe32a0ae16
owen <owenjharrison@protonmail.com>
2018-03-16 06:55:22 +0000

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  #resources :visitors, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
   
